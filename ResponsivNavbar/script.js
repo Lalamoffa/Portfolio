580c258c86a96207e54527b6fab02efa326399c0
Lalamoffa <lalememmedova1133@gmail.com>
2024-11-19 00:45:59 +0400

    yeni

--- ResponsivNavbar/script.js
+++
@@ -1,8 +0,0 @@
-// Toggle düyməsinə kliklədikdə menyunun açılıb-bağlanması
-const toggleButton = document.querySelector('.toggle-button');
-const navbarLinks = document.querySelector('.center ul');
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-    toggleButton.classList.toggle('active');
-});
